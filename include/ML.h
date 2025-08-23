ad73c5d17f708e7044dcba244625135006913561
Aibekin <aibekin_zh_007@mail.ru>
2025-08-24 01:14:14 +0500

    minimum working machine learning framework

---
+++ include/ML.h
@@ -0,0 +1,38 @@
+#ifndef ML_H
+#define ML_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <time.h>
+#include <math.h>
+#include <matrix/matrix.h>
+
+typedef struct
+{
+    size_t count;
+    c_matrix *ws, *bs, *as;
+} NN;
+
+#define NN_INPUT(nn) (nn).as[0]
+#define NN_OUTPUT(nn) (nn).as[(nn).count]
+
+#define ARRAY_LEN(xs) sizeof((xs)) / sizeof((xs)[0])
+
+float sigmoidf(float x);
+void mat_sigmoid(c_matrix *m);
+
+// size_t arch[] = {2, 2, 1};
+// NN nn = nn_alloc(arch, ARRAY_LEN(arch));
+
+NN alloc_nn(size_t *arch, size_t arch_count);
+void rand_nn(NN *nn, float low, float high);
+void print_nn(NN *nn, const char *name);
+#define PRINT_NN(nn) print_nn(nn, #nn);
+
+void forward_nn(NN *nn);
+float cost_nn(NN *nn, c_matrix *in, c_matrix *out);
+void diff_nn(NN *nn, NN *g, float eps, c_matrix *in, c_matrix *out);
+void learn_nn(NN *nn, NN *g, float rate);
+
+#endif
---
+++ src/ML.c
@@ -0,0 +1,155 @@
+#include "ML.h"
+
+float sigmoidf(float x)
+{
+    return 1.f / (1.f + expf(-x));
+}
+
+void mat_sigmoid(c_matrix *m)
+{
+    for (size_t i = 0; i < m->rows; ++i)
+    {
+        for (size_t j = 0; j < m->cols; ++j)
+        {
+            MAT_AT(m, i, j).value = sigmoidf(MAT_AT(m, i, j).value);
+        }
+    }
+}
+
+NN alloc_nn(size_t *arch, size_t arch_count)
+{
+    assert(arch_count > 0);
+
+    NN nn;
+    nn.count = arch_count - 1;
+
+    nn.ws = malloc(sizeof(*nn.ws) * nn.count);
+    assert(nn.ws != NULL);
+    nn.bs = malloc(sizeof(*nn.bs) * nn.count);
+    assert(nn.bs != NULL);
+    nn.as = malloc(sizeof(*nn.as) * (nn.count + 1));
+    assert(nn.as != NULL);
+
+    nn.as[0] = alloc_matrix(1, arch[0]);
+    for (size_t i = 1; i < arch_count; ++i)
+    {
+        nn.ws[i - 1] = alloc_matrix(nn.as[i - 1].cols, arch[i]);
+        nn.bs[i - 1] = alloc_matrix(1, arch[i]);
+        nn.as[i] = alloc_matrix(1, arch[i]);
+    }
+
+    return nn;
+}
+
+void rand_nn(NN *nn, float low, float high)
+{
+    for (size_t i = 0; i < nn->count; ++i)
+    {
+        nn->ws[i] = create_random(nn->ws[i].rows, nn->ws[i].cols, low, high);
+        nn->bs[i] = create_random(nn->bs[i].rows, nn->bs[i].cols, low, high);
+    }
+}
+
+void print_nn(NN *nn, const char *name)
+{
+    char buffer[256];
+    printf("%s = [\n", name);
+    for (size_t i = 0; i < nn->count; ++i)
+    {
+        snprintf(buffer, sizeof(buffer), "ws%zu", i);
+        print_matrix(&nn->ws[i], buffer, 4);
+        snprintf(buffer, sizeof(buffer), "bs%zu", i);
+        print_matrix(&nn->bs[i], buffer, 4);
+    }
+    printf("]\n");
+}
+
+void forward_nn(NN *nn)
+{
+    for (size_t i = 0; i < nn->count; ++i)
+    {
+        nn->as[i + 1] = multiply(&nn->as[i], &nn->ws[i]);
+        nn->as[i + 1] = addition(&nn->as[i + 1], &nn->bs[i]);
+        mat_sigmoid(&nn->as[i + 1]);
+    }
+}
+
+float cost_nn(NN *nn, c_matrix *in, c_matrix *out)
+{
+    assert(in->rows == out->rows);
+    assert(out->cols == NN_OUTPUT(*nn).cols);
+    size_t input_rows = in->rows;
+
+    float cost = 0;
+
+    for (size_t i = 0; i < input_rows; ++i)
+    {
+        c_matrix x = get_row(in, i);
+        c_matrix y = get_row(out, i);
+
+        copy_matrix(&NN_INPUT(*nn), &x);
+        forward_nn(nn);
+
+        size_t output_cols = out->cols;
+
+        for (size_t j = 0; j < output_cols; ++j)
+        {
+            float difference = MAT_AT(&NN_OUTPUT(*nn), 0, j).value - MAT_AT(&y, 0, j).value;
+            cost += difference * difference;
+        }
+    }
+
+    return cost / input_rows;
+}
+
+void diff_nn(NN *nn, NN *g, float eps, c_matrix *in, c_matrix *out)
+{
+    float saved;
+    float cost = cost_nn(nn, in, out);
+
+    for (size_t i = 0; i < nn->count; ++i)
+    {
+        for (size_t j = 0; j < nn->ws[i].rows; ++j)
+        {
+            for (size_t k = 0; k < nn->ws[i].cols; ++k)
+            {
+                saved = MAT_AT(&nn->ws[i], j, k).value;
+                MAT_AT(&nn->ws[i], j, k).value += eps;
+                MAT_AT(&g->ws[i], j, k).value = (cost_nn(nn, in, out) - cost) / eps;
+                MAT_AT(&nn->ws[i], j, k).value = saved;
+            }
+        }
+
+        for (size_t j = 0; j < nn->bs[i].rows; ++j)
+        {
+            for (size_t k = 0; k < nn->bs[i].cols; ++k)
+            {
+                saved = MAT_AT(&nn->bs[i], j, k).value;
+                MAT_AT(&nn->bs[i], j, k).value += eps;
+                MAT_AT(&g->bs[i], j, k).value = (cost_nn(nn, in, out) - cost) / eps;
+                MAT_AT(&nn->bs[i], j, k).value = saved;
+            }
+        }
+    }
+}
+
+void learn_nn(NN *nn, NN *g, float rate)
+{
+    for (size_t i = 0; i < 2; ++i)
+    {
+        for (size_t j = 0; j < nn->ws[i].rows; ++j)
+        {
+            for (size_t k = 0; k < nn->ws[i].cols; ++k)
+            {
+                MAT_AT(&nn->ws[i], j, k).value -= rate * MAT_AT(&g->ws[i], j, k).value;
+            }
+        }
+        for (size_t j = 0; j < nn->bs[i].rows; ++j)
+        {
+            for (size_t k = 0; k < nn->bs[i].cols; ++k)
+            {
+                MAT_AT(&nn->bs[i], j, k).value -= rate * MAT_AT(&g->bs[i], j, k).value;
+            }
+        }
+    }
+}
